d1de9e216e4038943229d9ae3413c397c5492a01
hironori sasaki <hrnr.ssk@gmail.com>
2020-10-09 00:00:22 +0900

    テストを実装

--- spec/factories/labels.rb
+++ spec/factories/labels.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :label do
-    name { "MyString" }
+    name { "sample1" }
+  end
+  factory :second_label, class:Label do
+    name { "sample2" }
+  end
+  factory :third_label, class:Label do
+    name { "sample3" }
+  end
+  factory :forth_label, class:Label do
+    name { "sample4" }
+  end
+  factory :fifth_label, class:Label do
+    name { "sample5" }
   end
 end
--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -1,32 +1,52 @@
 # 「FactoryBotを使用します」という記述
 FactoryBot.define do
-    # 作成するテストデータの名前を「task」とします
-    # （実際に存在するクラス名と一致するテストデータの名前をつければ、そのクラスのテストデータを自動で作成します）
-    factory :task do
-      name { 'Factoryで作ったデフォルトのタスク1' }
-      detail { 'Factoryで作ったデフォルトのコンテント１' }
-      deadline { '2020/08/10' }
-      status { 'Factoryで作ったデフォルトの状態１' }
-      priority { '高' }
-      user
+  # 作成するテストデータの名前を「task」とします
+  # （実際に存在するクラス名と一致するテストデータの名前をつければ、そのクラスのテストデータを自動で作成します）
+  factory :task do
+    name { 'Factoryで作ったデフォルトのタスク1' }
+    detail { 'Factoryで作ったデフォルトのコンテント１' }
+    deadline { '2020/08/10' }
+    status { 'Factoryで作ったデフォルトの状態１' }
+    priority { '高' }
+    user
+    after(:build) do |task|
+      label = create(:label)
+      task.labellings << build(:labelling, task: task, label: label)
     end
-    # 作成するテストデータの名前を「second_task」とします
-    # （存在しないクラス名の名前をつける場合、オプションで「このクラスのテストデータにしてください」と指定します）
-    factory :second_task, class: Task do
-      name { 'Factoryで作ったデフォルトのタスク2' }
-      detail { 'Factoryで作ったデフォルトのコンテント２' }
-      deadline { '2020/08/20' }
-      status { 'Factoryで作ったデフォルトの状態２' }
-      priority { '中' }
-      user
+    after(:build) do |task|
+      label = create(:second_label)
+      task.labellings << build(:labelling, task: task, label: label)
     end
+  end
+  # 作成するテストデータの名前を「second_task」とします
+  # （存在しないクラス名の名前をつける場合、オプションで「このクラスのテストデータにしてください」と指定します）
+  factory :second_task, class: Task do
+    name { 'Factoryで作ったデフォルトのタスク2' }
+    detail { 'Factoryで作ったデフォルトのコンテント２' }
+    deadline { '2020/08/20' }
+    status { 'Factoryで作ったデフォルトの状態２' }
+    priority { '中' }
+    user
+    after(:build) do |task|
+      label = create(:third_label)
+      task.labellings << build(:labelling, task: task, label: label)
+    end
+  end
 
-    factory :third_task, class: Task do
-      name { 'Factoryで作ったデフォルトのタスク3' }
-      detail { 'Factoryで作ったデフォルトのコンテント3' }
-      deadline { '2020/08/11' }
-      status { 'Factoryで作ったデフォルトの状態3' }
-      priority { '低' }
-      user
+  factory :third_task, class: Task do
+    name { 'Factoryで作ったデフォルトのタスク3' }
+    detail { 'Factoryで作ったデフォルトのコンテント3' }
+    deadline { '2020/08/11' }
+    status { 'Factoryで作ったデフォルトの状態3' }
+    priority { '低' }
+    user
+    after(:build) do |task|
+      label = create(:forth_label)
+      task.labellings << build(:labelling, task: task, label: label)
     end
+    after(:build) do |task|
+      label = create(:fifth_label)
+      task.labellings << build(:labelling, task: task, label: label)
+    end
+  end
 end
