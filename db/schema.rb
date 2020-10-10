4e7937d5cfe27ccc28711d824a90d3f7ff4f3832
hironori sasaki <hrnr.ssk@gmail.com>
2020-10-11 00:56:10 +0900

    Fix: labelsテーブルのnameとUsersテーブルのuser_nameとemailとpassword_digestとadminにもnull制約を付与

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_04_175043) do
+ActiveRecord::Schema.define(version: 2020_10_10_154942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2020_10_04_175043) do
   end
 
   create_table "labels", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -44,12 +44,12 @@ ActiveRecord::Schema.define(version: 2020_10_04_175043) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "user_name"
-    t.string "email"
-    t.string "password_digest"
+    t.string "user_name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin"
+    t.boolean "admin", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
