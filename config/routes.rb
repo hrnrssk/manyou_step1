8ee79cf700e2adffc3f5b74b0bddf540d755775f
hironori sasaki <hrnr.ssk@gmail.com>
2020-10-11 01:07:54 +0900

    Fix: resources :labelsをresources :tasksの直下に記載

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :labels
   get 'sessions/new'
   root to: 'tasks#index'
   resources :tasks
+  resources :labels
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
   namespace :admin do
