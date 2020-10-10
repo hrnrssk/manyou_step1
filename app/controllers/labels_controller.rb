99eb432d28d812fb396d82533812dcb9692cbbe0
hironori sasaki <hrnr.ssk@gmail.com>
2020-10-11 01:27:55 +0900

    Fix: app/controllers/labels_controller.rbの必要のないコメントを削除

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,26 +1,18 @@
 class LabelsController < ApplicationController
   before_action :set_label, only: [:show, :edit, :update, :destroy]
 
-  # GET /labels
-  # GET /labels.json
   def index
     @labels = Label.all
   end
 
-  # GET /labels/1
-  # GET /labels/1.json
   def show; end
 
-  # GET /labels/new
   def new
     @label = Label.new
   end
 
-  # GET /labels/1/edit
   def edit; end
 
-  # POST /labels
-  # POST /labels.json
   def create
     @label = Label.new(label_params)
 
@@ -35,8 +27,6 @@ class LabelsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /labels/1
-  # PATCH/PUT /labels/1.json
   def update
     respond_to do |format|
       if @label.update(label_params)
@@ -49,8 +39,6 @@ class LabelsController < ApplicationController
     end
   end
 
-  # DELETE /labels/1
-  # DELETE /labels/1.json
   def destroy
     @label.destroy
     respond_to do |format|
@@ -60,12 +48,10 @@ class LabelsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_label
       @label = Label.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def label_params
       params.require(:label).permit(:name)
     end
